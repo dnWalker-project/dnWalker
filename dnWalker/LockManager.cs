ab5aef0daa31d3508635597b70492b36de1627f3
Karel Frajták <kfrajtak@lekis.cz>
2020-02-18 14:18:38 +0100

    code cleanup

--- dnWalker/LockManager.cs
+++ dnWalker/LockManager.cs
@@ -15,109 +15,117 @@
  *
  */
 
-namespace MMC.State {
+namespace MMC.State
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using MMC.Data;
 
-	using System.Collections.Generic;
-	using System.Collections;
-	using System.Diagnostics;
-	using MMC.Data;
+    public static class LockManager
+    {
+        /// Constant for 'not owner'. Nullable types would be nice, but...
+        public const int NoThread = -1;
 
+        /// <summary>Check if an allocation has an associated locking thread. [const]</summary>
+        ///
+        /// <param name="obj">Reference to the allocation.</param>
+        /// <returns>True iff a thread owns the lock on this allocation.</returns>
+        public static bool IsLocked(ObjectReference obj, ExplicitActiveState cur)
+        {
+            DynamicAllocation alloc = cur.DynamicArea.Allocations[obj];
+            Debug.Assert(alloc != null, "Checking lock on null allocation.");
+            return alloc.Locked;
+        }
 
-    public class LockManager  {
+        /// <summary>Get the lock associated with an allocation.</summary>
+        /// <remarks>
+        /// This creates a new (empty) lock if none exists (done by the
+        /// Allocation class).
+        /// </remarks>
+        /// <param name="obj">Reference to the allocation.</param>
+        /// <returns>The associated lock (possibly new).</returns>
+        public static Lock GetLock(ObjectReference obj, ExplicitActiveState cur)
+        {
+            DynamicAllocation alloc = cur.DynamicArea.Allocations[obj];
+            Debug.Assert(alloc != null, "Getting lock on null allocation.");
+            return alloc.Lock;
+        }
 
-		/// Constant for 'not owner'. Nullable types would be nice, but...
-		public const int NoThread = -1;
+        /// <summary>Attempt to acquire the lock on an allocation for a thread.</summary>
+        /// <remarks>
+        /// One thread can own more than one lock on an allocation. To release
+        /// the lock completely the number of Release() calls should equal the
+        /// number of Acquire() calls.
+        /// </remarks>
+        /// <param name="obj">Reference to the allocation.</param>
+        /// <param name="thread_id">The number of the thread to acquire the lock for.</param>
+        /// <returns>True iff the lock was succesfully acquired.</returns>
+        public static bool Acquire(ObjectReference obj, int thread_id, ExplicitActiveState cur)
+        {
+            Lock l = GetLock(obj, cur);
+            bool retval = l.Owner == thread_id || l.Owner == NoThread;
+            if (retval)
+            {
+                l.Owner = thread_id;
+                l.Count++;
+            }
+            return retval;
+        }
 
-		/// <summary>Check if an allocation has an associated locking thread. [const]</summary>
-		///
-		/// <param name="obj">Reference to the allocation.</param>
-		/// <returns>True iff a thread owns the lock on this allocation.</returns>
-		public bool IsLocked(ObjectReference obj, ExplicitActiveState cur) {
+        public static bool IsAcquireable(ObjectReference obj, int thread_id, ExplicitActiveState cur)
+        {
+            Lock l = GetLock(obj, cur);
+            return l.Owner == thread_id || l.Owner == NoThread;
+        }
 
-			DynamicAllocation alloc = cur.DynamicArea.Allocations[obj];
-			Debug.Assert(alloc != null, "Checking lock on null allocation.");
-			return alloc.Locked;
-		}
+        /// <summary>Notify (all) waiting threads.</summary>
+        /// <remarks>
+        /// This moves one (or all, if all is true) thread from the waiting
+        /// queue to the ready queue.
+        /// </remarks>
+        /// <param name="obj">Reference to the allocation.</param>
+        /// <param name="all">Notify all waiting threads if true. Just one otherwise.</param>
+        public static void Pulse(ObjectReference obj, bool all, ExplicitActiveState cur)
+        {
+            Lock l = GetLock(obj, cur);
+            if (l.HasWaitQueue())
+            {
+                Queue<int> waitQueue = l.WaitQueue;
+                Queue<int> readyQueue = l.ReadyQueue;
+                int max = (all ? waitQueue.Count : 1);
+                while (max-- > 0 && waitQueue.Count > 0)
+                {
+                    readyQueue.Enqueue(waitQueue.Dequeue());
+                }
+            }
+        }
 
-		/// <summary>Get the lock associated with an allocation.</summary>
-		///
-		/// This creates a new (empty) lock if none exists (done by the
-		/// Allocation class).
-		///
-		/// <param name="obj">Reference to the allocation.</param>
-		/// <returns>The associated lock (possibly new).</returns>
-		public Lock GetLock(ObjectReference obj, ExplicitActiveState cur) {
-
-			DynamicAllocation alloc = cur.DynamicArea.Allocations[obj];
-			Debug.Assert(alloc != null, "Getting lock on null allocation.");
-			return alloc.Lock;
-		}
-
-		/// <summary>Attempt to acquire the lock on an allocation for a thread.</summary>
-		///
-		/// One thread can own more than one lock on an allocation. To release
-		/// the lock completely the number of Release() calls should equal the
-		/// number of Acquire() calls.
-		///
-		/// <param name="obj">Reference to the allocation.</param>
-		/// <param name="thread_id">The number of the thread to acquire the lock for.</param>
-		/// <returns>True iff the lock was succesfully acquired.</returns>
-		public bool Acquire(ObjectReference obj, int thread_id, ExplicitActiveState cur) {
-
-			Lock l = GetLock(obj, cur);
-			bool retval = l.Owner == thread_id || l.Owner == NoThread;
-			if (retval) {
-				l.Owner = thread_id;
-				l.Count++;
-			}
-			return retval;
-		}
-
-		public bool IsAcquireable(ObjectReference obj, int thread_id, ExplicitActiveState cur) {
-			Lock l = GetLock(obj, cur);
-			return l.Owner == thread_id || l.Owner == NoThread;
-		}
-
-		/// <summary>Notify (all) waiting threads.</summary>
-		///
-		/// This moves one (or all, if all is true) thread from the waiting
-		/// queue to the ready queue.
-		///
-		/// <param name="obj">Reference to the allocation.</param>
-		/// <param name="all">Notify all waiting threads if true. Just one otherwise.</param>
-		public void Pulse(ObjectReference obj, bool all, ExplicitActiveState cur) {
-
-			Lock l = GetLock(obj, cur);
-			if (l.HasWaitQueue()) {
-				Queue<int> waitQueue = l.WaitQueue;
-				Queue<int> readyQueue = l.ReadyQueue;
-				int max = (all ? waitQueue.Count : 1);
-				while (max-- > 0 && waitQueue.Count > 0)
-					readyQueue.Enqueue((int)waitQueue.Dequeue());
-			}
-		}
-
-		/// <summary>Release the lock on an allocation.</summary>
-		///
-		/// If the lock is completely released (i.e. the number of Release()
-		/// calls equals the number of Acquire() calls), the ownership of the
-		/// lock is automatically passed to the first thread in the ready
-		/// queue.
-		///
-		/// <param name="obj">Reference to the allocation.</param>
-		public void Release(ObjectReference obj, ExplicitActiveState cur) {
-
-			Lock l = GetLock(obj, cur);
-			l.Count--;
-			// If lock is fully released...
-			if (l.Count == 0) {
-				if (l.HasReadyQueue()) {
-					// Let first in line in the ready queue acquire the lock.
-					l.Owner = (int)l.ReadyQueue.Dequeue();
-					l.Count = 1;
-				} else
-					l.Owner = NoThread;
-			}
-		}
-	}
+        /// <summary>Release the lock on an allocation.</summary>
+        /// <remarks>
+        /// If the lock is completely released (i.e. the number of Release()
+        /// calls equals the number of Acquire() calls), the ownership of the
+        /// lock is automatically passed to the first thread in the ready
+        /// queue.
+        /// </remarks>
+        /// <param name="obj">Reference to the allocation.</param>
+        public static void Release(ObjectReference obj, ExplicitActiveState cur)
+        {
+            Lock l = GetLock(obj, cur);
+            l.Count--;
+            // If lock is fully released...
+            if (l.Count == 0)
+            {
+                if (l.HasReadyQueue())
+                {
+                    // Let first in line in the ready queue acquire the lock.
+                    l.Owner = l.ReadyQueue.Dequeue();
+                    l.Count = 1;
+                }
+                else
+                {
+                    l.Owner = NoThread;
+                }
+            }
+        }
+    }
 }
