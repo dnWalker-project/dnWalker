d88fdf7def9126174c2f4314009a523798774f78
Benjamin Hejl <hejl.benjamin@gmail.com>
2021-10-05 22:30:17 +0200

    basic + naive arguments cache setup TODO: Clear & Remove functions + incorporate to the Concolic.Explorer.Run() method

---
+++ dnWalker.Tests/Concolic/ArgumentsCacheTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dnWalker;
+using dnWalker.Symbolic;
+using dnWalker.Concolic;
+
+using Xunit;
+using FluentAssertions;
+
+namespace dnWalker.Tests.Concolic
+{
+
+    [Trait("Category", "Functionality")]
+    public class ArgumentsCacheTests
+    {
+        [Fact]
+        public void WhenTryingToAddNull_Throws()
+        {
+            ArgumentsCache cache = new ArgumentsCache(3);
+
+            IArg[] nullArgs = null;
+
+            Assert.Throws<ArgumentNullException>(() => cache.TryAdd(nullArgs));
+        }
+
+        [Fact]
+        public void WhenTryingToAddDifferntArgsNumber_Throws()
+        {
+            ArgumentsCache threeArgsCache = new ArgumentsCache(3);
+
+            IArg[] fourArgs = new IArg[4];
+
+            Assert.Throws<ArgumentException>(() => threeArgsCache.TryAdd(fourArgs));
+        }
+
+        [Fact]
+        public void AddingArgsToEmptyCache_Returns_True()
+        {
+            ArgumentsCache threeArgsCache = new ArgumentsCache(3);
+
+            IArg[] threeArgs = new IArg[]
+            {
+                SymbolicArgs.Arg("x", 5),
+                SymbolicArgs.Arg("y", 4),
+                SymbolicArgs.Arg("z", 3)
+            };
+
+            threeArgsCache.TryAdd(threeArgs).Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void AddingSameArgsToCache_Returns_False()
+        {
+            ArgumentsCache threeArgsCache = new ArgumentsCache(3);
+
+            IArg[] threeArgs = new IArg[]
+            {
+                SymbolicArgs.Arg("x", 5),
+                SymbolicArgs.Arg("y", 4),
+                SymbolicArgs.Arg("z", 3)
+            };
+
+            threeArgsCache.TryAdd(threeArgs);//.Should().BeTrue();
+            threeArgsCache.TryAdd(threeArgs).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(new object[] { 1, 2.5, "hello world" }, new object[] { 1, 2.5, "hi world"})]
+        public void AddingSimiliarArgsToCache_Returns_True(object[] args1, object[] args2)
+        {
+            ArgumentsCache threeArgsCache = new ArgumentsCache(3);
+
+            IArg[] threeArgs = new IArg[3];
+            threeArgs[0] = SymbolicArgs.Arg("x", args1[0]);
+            threeArgs[1] = SymbolicArgs.Arg("y", args1[1]);
+            threeArgs[2] = SymbolicArgs.Arg("z", args1[2]);
+            threeArgsCache.TryAdd(threeArgs);
+
+            threeArgs[0] = SymbolicArgs.Arg("x", args2[0]);
+            threeArgs[1] = SymbolicArgs.Arg("y", args2[1]);
+            threeArgs[2] = SymbolicArgs.Arg("z", args2[2]);
+            threeArgsCache.TryAdd(threeArgs).Should().BeTrue();
+        }
+    }
+}
---
+++ dnWalker/Concolic/ArgumentsCache.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnWalker.Concolic
+{
+    public class ArgumentsCache
+    {
+        private class ArgumentsCacheLayer
+        {
+            private readonly Dictionary<IArg, ArgumentsCacheLayer> _items = new Dictionary<IArg, ArgumentsCacheLayer>();
+            private readonly int _depth;
+
+            public ArgumentsCacheLayer(Int32 depth)
+            {
+                _depth = depth;
+            }
+
+            public bool TryAdd(IArg[] args, int index)
+            {
+                IArg currentArg = args[index];
+                if (args.Length - 1 == index && _items.ContainsKey(currentArg))
+                {
+                    // we are trying to add the last one and it is already in the layer => return false
+                    return false;
+                }
+
+                if (!_items.TryGetValue(currentArg, out ArgumentsCacheLayer nextLayer))
+                {
+                    nextLayer = new ArgumentsCacheLayer(_depth + 1);
+                    _items[currentArg] = nextLayer;
+                }
+
+                return index == args.Length - 1 || nextLayer.TryAdd(args, index + 1);
+            }
+
+            public bool Remove(IArg[] args, int index)
+            {
+                return false;
+            }
+
+            public bool Contains(IArg[] args, int index)
+            {
+                if (args.Length - 1 == index)
+                {
+                    // last item
+                    return _items.ContainsKey(args[index]);
+                }
+
+                if (_items.TryGetValue(args[index], out ArgumentsCacheLayer nextLayer))
+                {
+                    return nextLayer.Contains(args, index + 1);
+                }
+                return false;
+            }
+
+            public void Clear()
+            {
+                _items.Clear();
+            }
+        }
+
+        private readonly int _depth;
+        private readonly ArgumentsCacheLayer _baseLayer = new ArgumentsCacheLayer(0);
+
+        public Int32 Depth
+        {
+            get
+            {
+                return _depth;
+            }
+        }
+
+        public ArgumentsCache(Int32 depth)
+        {
+            _depth = depth;
+        }
+
+        public void Clear()
+        {
+            _baseLayer.Clear();
+        }
+
+        public bool TryAdd(IArg[] args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            if (args.Length != _depth)
+            {
+                throw new ArgumentException("Args array has wrong length", nameof(args));
+            }
+
+            return _baseLayer.TryAdd(args, 0);
+        }
+
+        public bool Remove(IArg[] args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            if (args.Length != _depth)
+            {
+                throw new ArgumentException("Args array has wrong length", nameof(args));
+            }
+
+            return _baseLayer.Remove(args, 0);
+        }
+
+        public bool Contains(IArg[] args)
+        {
+            if (args == null || args.Length != _depth)
+            {
+                return false;
+            }
+
+            return _baseLayer.Contains(args, 0);
+        }
+    }
+}
