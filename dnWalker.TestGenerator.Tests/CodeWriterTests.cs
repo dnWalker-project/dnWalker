b9e4b7bd3b686ee9dc3b301dea17e6a8c0104e86
Benjamin Hejl <hejl.benjamin@gmail.com>
2021-10-26 18:10:36 +0200

    basic codewriter and its tests

---
+++ dnWalker.TestGenerator.Tests/CodeWriterTests.cs
@@ -0,0 +1,193 @@
+﻿using FluentAssertions;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace dnWalker.TestGenerator.Tests
+{
+    public class CodeWriterTests
+    {
+        [Fact]
+        public void WriteUsing()
+        {
+            string namespaceName = "System";
+            //string expectedUsingString = $"using {namespaceName};\r\n";
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            codeWriter.WriteUsing(namespaceName);
+
+            writer.ToString().Should().Be(CodeTexts.Using_System);
+        }
+
+
+        [Fact]
+        public void WriteStatement()
+        {
+            string statement = "x = 5";
+            //string expectedStatement = $"{statement};\r\n";
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            codeWriter.WriteStatement(statement);
+
+            writer.ToString().Should().Be(CodeTexts.Assign_5_To_X);
+        }
+
+        [Fact]
+        public void WriteVariableDeclaration_WithInitialValue()
+        {
+            string variableType = "int";
+            string variableName = "x";
+            string variableValue = "5";
+
+            //string expected = $"{variableType} {variableName} = {variableValue};\r\n";
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            codeWriter.WriteVariableDeclaration(variableType, variableName, variableValue);
+
+            writer.ToString().Should().Be(CodeTexts.Declare_X_WithInitialValue);
+        }
+
+        [Fact]
+        public void WriteVariableDeclaration_WithoutInitialValue()
+        {
+            string variableType = "int";
+            string variableName = "x";
+
+            string expected = $"{variableType} {variableName};\r\n";
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            codeWriter.WriteVariableDeclaration(variableType, variableName, null);
+
+            writer.ToString().Should().Be(CodeTexts.Declare_X_WithoutInitialValue);
+        }
+
+        [Fact]
+        public void Test_DeclareEmptyClass()
+        {
+            string className = "MyClass";
+            //string expectedText = $"public class {className}\r\n{{\r\n}}\r\n";
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            using (IDisposable codeBlock = codeWriter.BeginClass(MemberModifiers.None, className))
+            { }
+
+            writer.ToString().Should().Be(CodeTexts.Declare_Empty_Class);
+        }
+
+        [Fact]
+        public void Test_DeclareEmptyClass_WithMethod()
+        {
+            string className = "MyClass";
+            string methodName = "MyMethod";
+            //string expectedText = $"public class {className}\r\n{{\r\n}}\r\n";
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            using (IDisposable classBlock = codeWriter.BeginClass(MemberModifiers.None, className))
+            { 
+                using (IDisposable methodBlock = codeWriter.BeginMethod(MemberModifiers.None, null, methodName))
+                { }
+            }
+
+            writer.ToString().Should().Be(CodeTexts.Declare_Class_WithMethod);
+        }
+
+        [Fact]
+        public void Test_DeclareClass_WithMethod_WithVariable()
+        {
+            string className = "MyClass";
+            string methodName = "MyMethod";
+            //string expectedText = $"public class {className}\r\n{{\r\n}}\r\n";
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            using (IDisposable classBlock = codeWriter.BeginClass(MemberModifiers.None, className))
+            {
+                using (IDisposable methodBlock = codeWriter.BeginMethod(MemberModifiers.None, null, methodName))
+                {
+                    codeWriter.WriteVariableDeclaration("int", "x", "5");
+                }
+            }
+
+            writer.ToString().Should().Be(CodeTexts.Declare_Class_WithMethod_WithVariable);
+        }
+
+        [Fact]
+        public void Test_WriteAttribute()
+        {
+            string attributeName = "MyAttribute";
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            codeWriter.WriteAttribute(attributeName);
+
+
+            writer.ToString().Should().Be(CodeTexts.WriteAttribute);
+        }
+
+        [Fact]
+        public void Test_WriteAttribute_WithArguments()
+        {
+            string attributeName = "MyAttribute";
+            string[] arguments = { "true", "5" };
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            codeWriter.WriteAttribute(attributeName, arguments);
+
+
+            writer.ToString().Should().Be(CodeTexts.WriteAttribute_WithArguments);
+        }
+
+        [Fact]
+        public void Test_WriteAttribute_WithArgumentsAndProperties()
+        {
+            string attributeName = "MyAttribute";
+            string[] arguments = { "true", "5" };
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            codeWriter.WriteAttribute(attributeName, arguments);
+
+
+            writer.ToString().Should().Be(CodeTexts.WriteAttribute_WithArguments);
+        }
+
+        [Fact]
+        public void Test_WriteAttribute_WithProperties()
+        {
+            string attributeName = "MyAttribute";
+            string[] propertyNames = { "SomeBooleanProperty", "SomeIntProperty" };
+            string[] propertyValues = { "false", "10" };
+
+            StringWriter writer = new StringWriter();
+
+            CodeWriter codeWriter = new CodeWriter(writer);
+            codeWriter.WriteAttribute(attributeName, propertyNames, propertyValues);
+
+
+            writer.ToString().Should().Be(CodeTexts.WriteAttribute_WithParameters);
+        }
+    }
+}
---
+++ dnWalker.TestGenerator/CodeWriter.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnWalker.TestGenerator
+{
+    [Flags]
+    public enum MemberModifiers
+    {
+        None      =  0,
+        Public    =  1,
+        Internal  =  2,
+        Protected =  4,
+        Private   =  8,
+        Static    = 16,
+        Readonly  = 32
+    }
+
+    public class CodeWriter : IDisposable
+    {
+        private struct CodeBlock : IDisposable
+        {
+            private readonly CodeWriter _codeWriter;
+
+            public CodeBlock(CodeWriter codeWriter)
+            {
+                _codeWriter = codeWriter;
+                _codeWriter.WriteLine("{");
+                _codeWriter.Indent();
+            }
+
+            public void Dispose()
+            {
+                _codeWriter.Outdent();
+                _codeWriter.WriteLine("}");
+            }
+        }
+
+
+        private readonly TextWriter _output;
+        private readonly bool _disposeOnClose;
+
+        private int _indent = 0;
+
+        public CodeWriter(TextWriter output, bool disposeOnClose = true)
+        {
+            _output = output;
+            _disposeOnClose = disposeOnClose;
+        }
+
+        public void Dispose()
+        {
+            if (_disposeOnClose)
+            {
+                _output.Dispose();
+            }
+        }
+
+        public IDisposable BeginCodeBlock()
+        {
+            return new CodeBlock(this);
+        }
+
+        public void Indent()
+        {
+            _indent++;
+        }
+
+        public void Outdent()
+        {
+            _indent = Math.Max(_indent - 1, 0);
+        }
+
+        public string TabString
+        {
+            get;
+            set;
+        } = "    ";
+
+        private void WriteLine(string line)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < _indent; ++i)
+            {
+                builder.Append(TabString);
+            }
+
+            builder.Append(line);
+            _output.WriteLine(builder);
+        }
+
+        public void WriteStatement(string statement)
+        {
+            WriteLine(statement + ";");
+        }
+
+        public void WriteUsing(string namespaceName)
+        {
+            WriteStatement("using " + namespaceName);
+        }
+
+        public void WriteVariableDeclaration(string variableType, string variableName, string initialValue)
+        {
+            if (initialValue != null)
+            {
+                WriteStatement($"{variableType} {variableName} = {initialValue}");
+            }
+            else
+            {
+                WriteStatement($"{variableType} {variableName}");
+            }
+        }
+
+        public IDisposable BeginClass(MemberModifiers modifiers, string className, params string[] inheritingTypes)
+        {
+            StringBuilder builder = new StringBuilder();
+            
+            if (inheritingTypes.Length == 0)
+            {
+                builder.AppendFormat("public class {0}", className);
+            }
+            else
+            {
+                builder.AppendFormat("public class {0}: {1}", className, string.Join(", ", inheritingTypes));
+            }
+
+            WriteLine(builder.ToString());
+            return BeginCodeBlock();
+        }
+
+        public IDisposable BeginMethod(MemberModifiers memberModifiers, string returnType, string methodName)
+        {
+            WriteLine($"public {returnType ?? "void"} {methodName}()");
+            return BeginCodeBlock();
+        }
+
+        public IDisposable BeginMethod(MemberModifiers memberModifiers, string returnType, string methodName, string[] argumentTypes, string[] argumentNames)
+        {
+            IEnumerable<String> paramDeclarations = Enumerable.Range(0, argumentTypes.Length).Select(i => $"{argumentTypes[i]} {argumentNames[i]}");
+            WriteLine($"public {returnType ?? "void"} {methodName}({String.Join(", ", paramDeclarations)})");
+            return BeginCodeBlock();
+        }
+
+        public void WriteAttribute(string attributeName)
+        {
+            WriteLine($"[{attributeName}]");
+        }
+
+        public void WriteAttribute(string attributeName, params string[] arguments)
+        {
+            WriteLine($"[{attributeName}({string.Join(", ", arguments)})]");
+        }
+
+        public void WriteAttribute(string attributeName, string[] propertyNames, string[] propertyValues)
+        {
+            IEnumerable<string> propertyDeclarations = Enumerable.Range(0, propertyNames.Length).Select(i => $"{propertyNames[i]} = {propertyValues[i]}");
+            WriteLine($"[{attributeName}({string.Join(", ", propertyDeclarations)})]");
+        }
+
+        public void WriteAttribute(string attributeName, string[] arguments, string[] propertyNames, string[] propertyValues)
+        {
+            IEnumerable<string> propertyDeclarations = Enumerable.Range(0, propertyNames.Length).Select(i => $"{propertyNames[i]} = {propertyValues[i]}");
+            WriteLine($"[{attributeName}({string.Join(", ", arguments)}, {string.Join(", ", propertyDeclarations)})]");
+        }
+    }
+}
