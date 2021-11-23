1d81e8e3b0944cc41588a9e6a09a2691f55d5eae
Benjamin Hejl <hejl.benjamin@gmail.com>
2021-11-23 13:13:33 +0100

    removed commented sections in AssemblyInfo

--- dnWalker.Tests/Properties/AssemblyInfo.cs
+++ dnWalker.Tests/Properties/AssemblyInfo.cs
@@ -2,19 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-//[assembly: AssemblyTitle("dnWalker.Tests")]
-//[assembly: AssemblyDescription("")]
-//[assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("")]
-//[assembly: AssemblyProduct("dnWalker.Tests")]
-//[assembly: AssemblyCopyright("Copyright ©  2020")]
-//[assembly: AssemblyTrademark("")]
-//[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("6746023d-0f86-464e-9f47-1d50efa731cc")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
--- dnWalker/Properties/AssemblyInfo.cs
+++ dnWalker/Properties/AssemblyInfo.cs
@@ -6,23 +6,6 @@ using System.Runtime.InteropServices;
 // values defined in project properties. For details of which attributes are included
 // and how to customise this process see: https://aka.ms/assembly-info-properties
 
-/*[assembly: AssemblyTitle ("MoonWalker")]
-[assembly: AssemblyDescription ("Model Checker for CIL bytecode programs")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyProduct ("MoonWalker")]
-[assembly: AssemblyVersion ("1.0.1.0")]
-[assembly: AssemblyCopyright("(C) 2008, University of Twente - Formal Methods and Tools group")]
-[assembly: AssemblyCulture ("")]*/
-
-/*[assembly: CLSCompliant (false)]
-[assembly: ComVisible (false)]*/
-
-// [assembly: AssemblyKeyFile("MoonWalkerKey.snk")]
-// [assembly: AssemblyCompanyAttribute("University of Twente")]
-
-
-
-
 // Setting ComVisible to false makes the types in this assembly not visible to COM
 // components.  If you need to access a type in this assembly from COM, set the ComVisible
 // attribute to true on that type.
