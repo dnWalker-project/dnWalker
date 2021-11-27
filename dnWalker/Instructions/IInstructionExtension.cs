7b1858ff86baaaafca845c9fa15a93072a7dcbbc
benjamin-hejl <hejl.benjamin@gmail.com>
2021-11-27 16:15:07 +0100

    added extension point to the instructions

---
+++ dnWalker/Instructions/IInstructionExtension.cs
@@ -0,0 +1,50 @@
+﻿using dnlib.DotNet.Emit;
+
+using MMC.InstructionExec;
+using MMC.State;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnWalker.Instructions
+{
+    public interface IInstructionExtension
+    {
+        /// <summary>
+        /// Determines wheter the extension can execute instruction with the supplied <paramref name="code"/>.
+        /// </summary>
+        public bool CanExecute(Code code);
+
+        /// <summary>
+        /// Initializes inner state of the extension for the instruction. Must not edit cur.
+        /// Will always be invoked everytime the extended instruction is executed.
+        /// </summary>
+        /// <param name="instruction"></param>
+        /// <param name="cur"></param>
+        public void PreExecute(InstructionExecBase instruction, ExplicitActiveState cur);
+
+
+        /// <summary>
+        /// Tries to execute the operation in place of the default execution. 
+        /// It may not be invoked when the extended instruction is executed - another extension with higher priority can execute the instruction before.
+        /// </summary>
+        /// <param name="instruction"></param>
+        /// <param name="cur"></param>
+        /// <param name="retValue"></param>
+        /// <returns><code>true</code> if the operation was executed, <code>false</code> if execution did not proceed. If returns <code>false</code> the <see cref="ExplicitActiveState.CallStack"/> and <see cref="ExplicitActiveState.EvalStack"/> remains unchanged.</returns>
+        public bool TryExecute(InstructionExecBase instruction, ExplicitActiveState cur, [NotNullWhen(true)] out IIEReturnValue retValue);
+
+        /// <summary>
+        /// Can do some operation on the eval and call stack after the execution iself - attach some metadata, do statistics etc. 
+        /// Will always be invoked everytime the extended instruction is executed.
+        /// </summary>
+        /// <param name="instruction"></param>
+        /// <param name="cur"></param>
+        /// <param name="retValue"></param>
+        public void PostExecute(InstructionExecBase instruction, ExplicitActiveState cur, IIEReturnValue retValue);
+    }
+}
---
+++ dnWalker/Instructions/Instructions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnWalker.Instructions
+{
+    // TODO: put "default" concrete and symbolic behavior of instruction in here
+    // it would be probably nice to extract the symbolic behavior into an extension, but since other extension may depend on it, it should be in the default, not extendend, behavior, at least for now, until more complex extensions would be designed
+}
