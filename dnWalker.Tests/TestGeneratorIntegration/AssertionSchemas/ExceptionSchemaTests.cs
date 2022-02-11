b113feb0d56a972fbd0f238ba8e5571bdec597de
benjamin-hejl <hejl.benjamin@gmail.com>
2022-02-11 17:37:06 +0100

    test base class

---
+++ dnWalker.Tests/TestGeneratorIntegration/AssertionSchemas/ExceptionSchemaTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit.Abstractions;
+
+namespace dnWalker.Tests.TestGeneratorIntegration.AssertionSchemas
+{
+    public class ExceptionSchemaTests : IntegrationTestBase
+    {
+        public ExceptionSchemaTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+
+        }
+    }
+}
---
+++ dnWalker.Tests/TestGeneratorIntegration/IntegrationTestBase.cs
@@ -0,0 +1,26 @@
+﻿using dnWalker.Tests.ExampleTests;
+using dnWalker.TypeSystem;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit.Abstractions;
+
+namespace dnWalker.Tests.TestGeneratorIntegration
+{
+    public class IntegrationTestBase : ExamplesTestBase
+    {
+        protected static readonly string AssemblyFilePath = string.Format(ExamplesAssemblyFileFormat, "Release");
+
+        public IntegrationTestBase(ITestOutputHelper testOutputHelper) : base(testOutputHelper, new DefinitionProvider(TestBase.GetDefinitionContext(AssemblyFilePath)))
+        {
+
+        }
+
+
+
+    }
+}
