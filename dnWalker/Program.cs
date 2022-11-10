8500937472866962086285a3fb216b3de7f51559
benjamin-hejl <hejl.benjamin@gmail.com>
2022-11-10 12:44:20 +0100

    clean up old (MoonWalker) CLI + add basic setup (Program::Main(string[])) method

---
+++ dnWalker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnWalker
+{
+    internal class Program
+    {
+        internal static int Main(string[] args)
+        {
+            Console.WriteLine("dnWalker");
+
+            return 0;
+        }
+    }
+}
