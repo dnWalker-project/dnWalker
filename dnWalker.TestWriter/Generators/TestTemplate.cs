a724a6031a13127a12b7904d8e9894e8c0936cd1
benjamin-hejl <hejl.benjamin@gmail.com>
2022-11-16 12:26:56 +0100

    test template extensions

---
+++ dnWalker.TestWriter/Generators/TestTemplate.cs
@@ -0,0 +1,37 @@
+﻿using dnWalker.TestWriter.Generators.Act;
+using dnWalker.TestWriter.Generators.Arrange;
+using dnWalker.TestWriter.Generators.Assert;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnWalker.TestWriter.Generators
+{
+    internal class TestTemplate : ITestTemplate
+    {
+        public TestTemplate(IReadOnlyList<IArrangePrimitives> arrangeWriters, IReadOnlyList<IActPrimitives> actWriters, IReadOnlyList<IAssertPrimitives> assertWriters)
+        {
+            ArrangeWriters = arrangeWriters;
+            ActWriters = actWriters;
+            AssertWriters = assertWriters;
+        }
+
+        public IReadOnlyList<IArrangePrimitives> ArrangeWriters
+        {
+            get;
+        }
+
+        public IReadOnlyList<IActPrimitives> ActWriters
+        {
+            get;
+        }
+
+        public IReadOnlyList<IAssertPrimitives> AssertWriters
+        {
+            get;
+        }
+    }
+}
