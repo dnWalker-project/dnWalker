c854ddb0349ddb4ddf76cf3aeb67d7e15b9a514f
benjamin-hejl <hejl.benjamin@gmail.com>
2022-11-18 16:24:53 +0100

    return value schema tests setup

---
+++ dnWalker.TestWriter.Tests/Generators/Schemas/ReturnValue/ReturnValueTestSchemaTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit.Abstractions;
+
+namespace dnWalker.TestWriter.Tests.Generators.Schemas.ReturnValue
+{
+    file class TestClass
+    {
+        public int ReturnInteger() { return 1; }
+        public static int StaticReturnInteger() { return 1; }
+
+
+        public string ReturnString() { return "Hello worl"; }
+        public static int StaticReturnString() { return 1; }
+    }
+
+    public class ReturnValueTestSchemaTests : TestSchemaTestBase
+    {
+        public ReturnValueTestSchemaTests(ITestOutputHelper textOutput) : base(textOutput)
+        {
+        }
+
+
+    }
+}
